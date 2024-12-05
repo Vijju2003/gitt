8eb52f668dba606f5d417f6f21ae733d590ee3db
root <root@ip-172-31-13-70.ap-south-1.compute.internal>
2024-12-05 11:47:51 +0000

    i have made sme changes

---
+++ app.java
@@ -0,0 +1,8 @@
+public class StringUtils {  
+    public String concatenate(String str1, String str2) {  
+        return str1 + str2;  
+    }  
+    public String concatenate(String str1, String str2, String str3) {  
+        return str1 + str2 + str3;  
+    }  
+}  
